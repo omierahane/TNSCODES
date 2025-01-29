ca524fd842e7aea3ea25d18f1dddaa88392866e1
omkar <omkar@192.168.0.107>
2025-01-29 14:00:34 +0530

    Day 01

---
+++ Day1/src/com/tnsday1/FirstProg.java
@@ -0,0 +1,22 @@
+package com.tnsday1;
+
+public class FirstProg {
+
+	public static void main(String args[]) {
+		int n=100;
+		for(int i=1;i<=n;i++) {
+			
+//			if(i%2==0) {
+//				System.out.println(i);
+//			}
+			
+			
+			for (int j=1;j<=n;j++) {
+				System.out.print("*");			
+				}
+		    
+		System.out.println();
+	}
+	}
+
+}
