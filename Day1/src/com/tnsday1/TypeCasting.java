a2d6e1a34f10ba2571b810959a8cc78563a783c4
omkar <omkar@Omkar>
2025-01-30 15:46:29 +0530

    add thesee

---
+++ Day1/src/com/tnsday1/TypeCasting.java
@@ -0,0 +1,53 @@
+package com.tnsday1;
+
+public class TypeCasting {
+	public static void main(String args[]) {
+		byte b=10;
+		int i=b;
+		System.out.println(i);
+		
+		
+		
+		
+		float f=100/3f;
+		
+		double d=f;
+		
+		System.out.println(f);	
+		// TODO Auto-generated method stub
+
+		
+		char ch='A';
+		
+		int i1=ch;
+		
+		
+		System.out.println(i1);
+
+	
+	
+//	overloading type caSTING
+		
+		
+	double f1=10.521f;
+	
+	long f11=(long) f1;
+	System.out.println(f11);
+	
+	long abc=9876543210l;
+	int i12=(int)abc;
+	
+	System.out.println(i12);
+	
+	
+	int xyz=130;
+	byte qwe =(byte)xyz;
+	
+	System.out.println(qwe);
+	
+	
+	
+	
+		}
+
+}
