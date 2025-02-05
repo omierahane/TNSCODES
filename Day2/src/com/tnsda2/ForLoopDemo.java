55b085efa071e1d10b4133d1d23f1cef288421ba
omkar <omkar@Omkar>
2025-02-05 20:47:09 +0530

    navin

---
+++ Day2/src/com/tnsda2/ForLoopDemo.java
@@ -0,0 +1,16 @@
+package com.tnsda2;
+
+
+public class ForLoopDemo {
+
+	public static void main(String[] args) {
+	
+		
+		for(int i=1; i<=100; i++)
+		{
+			System.out.println("Value of i : "+ i);
+		}
+
+	}
+
+}
---
+++ Day2/src/com/tnsda2/WhileLoopDemo.java
@@ -0,0 +1,20 @@
+package com.tnsda2;
+
+public class WhileLoopDemo {
+
+
+		public static void main(String[] args) {
+			
+			
+			int i=100;
+			
+			while(i<=100)
+			{
+				System.out.println(i);
+				i++;
+			}
+
+		}
+
+	}
+
